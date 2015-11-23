faf1a842d567d2da6551702c6d56da15f5850407
tesi fastflow dpi marcod <desensi@pianosa.di.unipi.it>
2015-11-23 14:45:20 +0100

    [IMP] Improved execution script

--- experiments/dynamic_reconfiguration/run.sh
+++ experiments/dynamic_reconfiguration/run.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-STRATEGIES=('cores_conservative' 'power_conservative' 'gov_ondemand' 'gov_conservative' 'gov_performance')
-rm -rf reconfigurations_count.txt
-rm -rf workers_per_second.txt
-rm -rf watts_per_second.txt
 
-strategy_id=0
-for strategy in "${STRATEGIES[@]}"
+SIG=signatures.1781
+HOST='pianosa'
+BLOCKING=('nonblocking' 'blocking')
+STRATEGIES=('corescons' 'reconf' 'ondemand' 'conservative' 'performance')
+
+for b in "${BLOCKING[@]}"
 do
-	./dynamic_reconfiguration signatures.example ../../../test_pcap_ok.pcap 1 $strategy_id
+    rm -rf ${HOST}_${BLOCKING}_reconfigurations_count.txt
+    rm -rf ${HOST}_${BLOCKING}_workers_per_second.txt
+    rm -rf ${HOST}_${BLOCKING}_watts_per_second.txt
+    rm -rf ${HOST}_${BLOCKING}_avg_rho.txt
+    strategy_id=1 # We avoid the coresconf strategy
+    for strategy in "${STRATEGIES[@]}"
+    do
+	./dynamic_reconfiguration ${SIG} ../../../test_pcap_ok.pcap 1 $strategy_id
 	RECONFIGURATIONS=$(cat stats.txt | tail -n +13 | cut -f 2 | cut -d ',' -f 1 | uniq -c | wc -l)
-	echo ${strategy} $RECONFIGURATIONS >> reconfigurations_count.txt
+	echo ${strategy} $RECONFIGURATIONS >> ${HOST}_${BLOCKING}_reconfigurations_count.txt
 	WORKERS_PER_SECOND=$(cat stats.txt  | tail -n +13 | cut -f 2 | cut -d ',' -f 1 | uniq -c | awk '{ print $1 * $2 }' | awk '{s+=$1} END {print s/3715}')
-	echo ${strategy} $WORKERS_PER_SECOND >> workers_per_second.txt
+	echo ${strategy} $WORKERS_PER_SECOND >> ${HOST}_${BLOCKING}_workers_per_second.txt
 	WATTS_PER_SECOND=$(cat stats.txt  | tail -n +13 |  cut -f 5 | awk '{s+=$1} END {print s/3715}')
-	echo ${strategy} $WATTS_PER_SECOND >> watts_per_second.txt
+	echo ${strategy} $WATTS_PER_SECOND >> ${HOST}_${BLOCKING}_watts_per_second.txt
         RHO=$(cat stats.txt  | tail -n +13 |  cut -f 10 | awk '{s+=$1} END {print s/3715}')
-        echo ${strategy} $RHO >> avg_rho.txt
-	mv stats.txt stats_${strategy}.txt
+        echo ${strategy} $RHO >> ${HOST}_${BLOCKING}_avg_rho.txt
+	mv stats.txt ${HOST}_${BLOCKING}_${strategy}_${SIG}.txt
 	((strategy_id++))	
+    done
 done
