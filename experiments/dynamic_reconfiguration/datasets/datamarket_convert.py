53b6b10119c157ff6f9d7d0aa3c32b3b160e616d
Daniele De Sensi <d.desensi.software@gmail.com>
2016-01-04 16:40:23 +0100

    [ADD] Added datasets folder for dynamic reconfiguration experiments.

---
+++ experiments/dynamic_reconfiguration/datasets/datamarket_convert.py
@@ -0,0 +1,16 @@
+firstline = 1
+day = "2005-06-08"
+sampledurationsecs = 300
+scalingfactor = 1000.0
+
+with open("datamarket_orig.csv") as f:
+    for line in f:
+        if firstline == 1:
+            firstline = 0
+        else:
+            fields = line.split(',')
+            if len(fields) > 1 and day in fields[0]:
+                ratebits = int(fields[1].strip())
+                ratepktspersec = ((ratebits/8.0) / 1400.0) * scalingfactor
+                print str(ratepktspersec) + " " + str(sampledurationsecs)
+
