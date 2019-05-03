299bb37672c6d0111972c55bfedef579006c4df3
Daniele De Sensi <d.desensi.software@gmail.com>
2019-05-03 12:17:17 +0200

    [IMP] Still working on Sphinx documentation.

--- docs/Doxyfile.in.c
+++
@@ -1,8 +0,0 @@
-OUTPUT_DIRECTORY       = .
-INPUT                  = ../include/peafowl/peafowl.h
-OPTIMIZE_OUTPUT_FOR_C  = YES
-PROJECT_NAME           = Peafowl
-GENERATE_XML           = YES
-GENERATE_HTML          = NO
-GENERATE_LATEX         = NO
-XML_OUTPUT             = _build/doxyxml_c
